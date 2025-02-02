da93fb3872b285702268c8ee8ff9639abddcbc16
SACHIN KUAR <sachinkumar.public@gmail.com>
2025-02-02 12:08:45 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is a java program fo jenkins");
+        System.out.println("Output for java Program");
+        System.out.println(new Date());
+    }
+}
